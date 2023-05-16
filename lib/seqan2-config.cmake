0da9a8f8285687c4b61d4cf9a27406b715823237
Enrico Seiler <enrico.seiler@hotmail.de>
2023-05-16 17:03:31 +0200

    [INFRA] Some more configuring

--- lib/seqan2-config.cmake
+++ lib/seqan2-config.cmake
@@ -8,15 +8,19 @@
 cmake_minimum_required (VERSION 3.12)
 
 if (TARGET seqan2::seqan2)
-    return()
-endif()
+    return ()
+endif ()
 
+find_package (OpenMP QUIET)
 find_package (ZLIB REQUIRED)
+find_package (BZip2 REQUIRED)
 
-add_library(seqan2 INTERFACE)
-target_include_directories(seqan2 INTERFACE SYSTEM
-    ${CMAKE_CURRENT_LIST_DIR}/seqan/include
-)
-target_link_libraries(seqan2 INTERFACE ${ZLIB_LIBRARIES} ${OPENMP_LIBRARIES})
-target_compile_definitions(seqan2 INTERFACE -DSEQAN_HAS_ZLIB)
-add_library(seqan2::seqan2 ALIAS seqan2)
+add_library (seqan2 INTERFACE)
+target_include_directories (seqan2 INTERFACE SYSTEM ${CMAKE_CURRENT_LIST_DIR}/seqan/include)
+if (OpenMP_FOUND)
+    target_link_libraries (seqan2 INTERFACE OpenMP::OpenMP_CXX)
+    target_compile_definitions (seqan2 INTERFACE -DSEQAN_HAS_OPENMP)
+endif ()
+target_link_libraries (seqan2 INTERFACE ZLIB::ZLIB BZip2::BZip2)
+target_compile_definitions (seqan2 INTERFACE -DSEQAN_HAS_ZLIB -DSEQAN_HAS_BZIP2)
+add_library (seqan2::seqan2 ALIAS seqan2)
